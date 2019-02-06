35ba4a1a4eb682168dca7be4970073557c15fb33
m20i02m <m20i02m@yahoo.ca>
2019-02-06 12:03:58 +0100

    mise en place d'un json pour la map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,17 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var jsonMap= {
+	lat: 48.8603,
+	lng: 2.443080000000009,
+	zoom: 15,
+	textPopup: "LE CHATEAU DE MES RÊVES, Montreuil"
+	};
+////
+var markerLatLng= [jsonMap.lat, jsonMap.lng];
+var map = L.map('map').setView(markerLatLng, jsonMap.zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+L.marker(markerLatLng).addTo(map)
+    .bindPopup(jsonMap.textPopup);
+////
