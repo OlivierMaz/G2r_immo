396e0d4f9b9d9a270f4cfcea9bba373ada7cafe6
m20i02m <m20i02m@yahoo.ca>
2019-02-07 07:41:09 +0100

    two divs content are now updated from a json object

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,17 +1,13 @@
-var jsonMap= {
-	lat: 48.8603,
-	lng: 2.443080000000009,
-	zoom: 15,
-	textPopup: "LE CHATEAU DE MES RÊVES, Montreuil"
-	};
-////
-var markerLatLng= [jsonMap.lat, jsonMap.lng];
-var map = L.map('map').setView(markerLatLng, jsonMap.zoom);
+$(document).ready(function () {
+// Now we'are about to create a leaflet map
+var markerLatLng= [jsonMap.lat, jsonMap.lng]; // Start coordinates
+var map = L.map('map').setView(markerLatLng, jsonMap.zoom); // Show a leaflet map centered on the data provided i nthe json map 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+}).addTo(map); // Select openstreet map as the default provider for all the tiles to be displayed
 
+// Now we'll create a marker (blue color by default)
 L.marker(markerLatLng).addTo(map)
-    .bindPopup(jsonMap.textPopup);
-////
+    .bindPopup(jsonMap.textPopup);// add a popup window on the marker - will be shown when the user clicks the marker
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-//todo
+var jsonMap;
+$(document).ready(function () {
+  // Handler for .ready() called.
+  console.log("abcd");
+  //alert("defg");
+  var jsonDataProperty= {
+	lat: 48.8603,
+	lng: 2.443080000000009,
+	zoom: 15,
+	page_title: "Achat de château de 1500m² à Montreuil - Rue de Stalingrad 0",
+	castle_name: "Chateau de Breuilmoulin 0"
+	};
+    // replace the contents of the div with the json text
+    $('#castle_name').html(jsonDataProperty.castle_name);
+	$('#page_title').html(jsonDataProperty.page_title);
+	// now we need to prepare a json object for the map creation
+	jsonMap= {
+	lat: jsonDataProperty.lat,
+	lng: jsonDataProperty.lng,
+	zoom: jsonDataProperty.zoom,
+	textPopup: jsonDataProperty.page_title
+	};
+});
